74922be695d07e2335c46d8f15d0a9f1b071e950
Artur Borecki <me@pufereq.pl>
2025-07-25 19:59:49 +0200

    refactor(dunst): rewrite dunst_play_sound.sh

--- .config/dunst/dunst_play_sound.sh
+++ .config/dunst/dunst_play_sound.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-# script appname summary body icon urgency
+#!/usr/bin/bash
 
-if [ "$1" = "Spotify" ]; then
-    exit
+exclude_apps="Spotify;Volume Control;Wallpaper"
+
+if [[ ";$exclude_apps;" == *";$DUNST_APP_NAME;"* ]]; then
+    exit 0
 fi
 
-if [ "$5" == "CRITICAL" ]; then
-    mpv --no-video /usr/share/sounds/oxygen/stereo/dialog-error-veryserious.ogg &
-elif [ "$5" == "NORMAL" ]; then
-    mpv --no-video /usr/share/sounds/oxygen/stereo/bell.ogg &
-elif [ "$5" == "LOW" ]; then
-    mpv --no-video /usr/share/sounds/oxygen/stereo/message-lowpriority.ogg &
-else
-    mpv --no-video /usr/share/sounds/oxygen/stereo/bell.ogg &
-fi
-echo "$1 $2 $3 $4 $5"
+sound=""
+case "$5" in
+"CRITICAL") sound="/usr/share/sounds/oxygen/stereo/dialog-error-veryserious.ogg" ;;
+"NORMAL") sound="/usr/share/sounds/oxygen/stereo/bell.ogg" ;;
+"LOW") sound="/usr/share/sounds/oxygen/stereo/message-lowpriority.ogg" ;;
+*) sound="/usr/share/sounds/oxygen/stereo/bell.ogg" ;;
+esac
 
+echo "Playing sound: $sound"
+mpv --no-video "$sound" &
