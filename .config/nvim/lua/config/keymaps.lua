d1b7c146222584e93580606b90488bc59930d7d2
Artur Borecki <me@pufereq.pl>
2025-08-20 13:59:05 +0200

    feat(nvim): remove unfunny amongus junk on arrowxs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -21,51 +21,7 @@ vim.keymap.set({ "n", "i", "v" }, "<Right>", function()
   print("➡️ Try l, queen")
 end)
 
-local api = vim.api
-local opts = { noremap = true, silent = true }
-
-local amongus_schlong = {
-  "⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⠀⠀⠀⢰⡿⠋⠁⠀⠀⠈⠉⠙⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⠀⠀⢀⣿⠇⠀⢀⣴⣶⡾⠿⠿⠿⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⣀⣀⣸⡿⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⣾⡟⠛⣿⡇⠀⠀⢸⣿⣿⣷⣤⣤⣤⣤⣶⣶⣿⠇⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀",
-  "⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣆",
-  "⢸⣿⠀⢸⣿⡇⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⣿⡇⣀⣠⣴⣾⣮⣝⠿⠿⠿⣻⡟",
-  "⢸⣿⠀⠘⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠉⠀",
-  "⠸⣿⠀⠀⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀",
-  "⠀⠻⣷⣶⣿⣇⠀⠀⠀⢠⣼⣿⣿⣿⣿⣿⣿⣿⣛⣛⣻⠉⠁⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⠀⠀⢸⣿⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-  "⠀⠀⠀⠀⠀⠙⠛⠛⠛⠋⠁⠀⠙⠻⠿⠟⠋⠑⠛⠋⠀",
-}
-
-local function show_amogus()
-  local buf = api.nvim_create_buf(false, true)
-  api.nvim_buf_set_lines(buf, 0, -1, false, amongus_schlong)
-
-  local win = api.nvim_open_win(buf, false, {
-    relative = "editor",
-    width = 32,
-    height = #amongus_schlong,
-    row = math.floor((vim.o.lines - #amongus_schlong + 2) / 2),
-    col = math.floor((vim.o.columns - 30) / 2),
-    style = "minimal",
-    border = "shadow",
-  })
-
-  vim.defer_fn(function()
-    if api.nvim_win_is_valid(win) then
-      api.nvim_win_close(win, true)
-      api.nvim_buf_delete(buf, { force = true })
-    end
-  end, 1200) -- give it time to emotionally scar you
-end
-
--- map all arrow keys to this madness
-for _, key in ipairs({ "<Up>", "<Down>", "<Left>", "<Right>" }) do
-  vim.keymap.set({ "n", "i", "v" }, key, function()
-    show_amogus()
-    vim.cmd("echohl ErrorMsg | echo '🚨 ARROW KEY?! SUS. USE HJKL DUMBASS.' | echohl None")
-  end, opts)
-end
+vim.keymap.set("n", "<leader>v", function()
+  vim.cmd("split | terminal")
+  vim.cmd("norm i")
+end, { desc = "Split Terminal" })
