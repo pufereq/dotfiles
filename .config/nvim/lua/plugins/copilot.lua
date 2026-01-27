43feea0517e0ebe5ecf1d844bb460be07346ab38
Artur Borecki <me@pufereq.pl>
2026-01-27 19:53:29 +0100

    feat(nvim): use copilot.native

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,57 @@
 -- disable telemetry for copilot
 return {
-  "zbirenbaum/copilot.lua",
-  lazy = false,
-  priority = 1000,
-  build = ":Copilot auth",
+  "neovim/nvim-lspconfig",
   opts = {
-    panel = { enabled = false },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      hide_during_completion = true,
-      debounce = 75,
-    },
-    server_opts_overrides = {
-      settings = {
-        telemetry = {
-          telemetryLevel = "off",
+    servers = {
+      copilot = {
+        -- stylua: ignore
+        keys = {
+          {
+            "<M-]>",
+            function() vim.lsp.inline_completion.select({ count = 1 }) end,
+            desc = "Next Copilot Suggestion",
+            mode = { "i", "n" },
+          },
+          {
+            "<M-[>",
+            function() vim.lsp.inline_completion.select({ count = -1 }) end,
+            desc = "Prev Copilot Suggestion",
+            mode = { "i", "n" },
+          },
+        },
+        settings = {
+          telemetry = {
+            telemetryLevel = "off",
+          },
         },
       },
+    },
+    setup = {
+      copilot = function()
+        vim.schedule(function()
+          vim.lsp.inline_completion.enable()
+        end)
+        -- Accept inline suggestions or next edits
+        LazyVim.cmp.actions.ai_accept = function()
+          return vim.lsp.inline_completion.get()
+        end
+
+        if not LazyVim.has_extra("ai.sidekick") then
+          vim.lsp.config("copilot", {
+            handlers = {
+              didChangeStatus = function(err, res, ctx)
+                if err then
+                  return
+                end
+                status[ctx.client_id] = res.kind ~= "Normal" and "error" or res.busy and "pending" or "ok"
+                if res.status == "Error" then
+                  LazyVim.error("Please use `:LspCopilotSignIn` to sign in to Copilot")
+                end
+              end,
+            },
+          })
+        end
+      end,
     },
   },
 }
