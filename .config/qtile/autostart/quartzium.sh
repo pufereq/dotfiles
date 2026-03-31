db8fe23aa4b9367aa9a6cdd13305735ae13a2ac7
Artur Borecki <me@pufereq.pl>
2026-03-31 13:52:37 +0200

    feat(autostart/quartzium): use gpu-screen-recorder instead of replay-sorcery

--- .config/qtile/autostart/quartzium.sh
+++ .config/qtile/autostart/quartzium.sh
@@ -1,5 +1,16 @@
 # replay manager
-systemctl --user start replay-sorcery &
+# systemctl --user start replay-sorcery &
+gpu-screen-recorder \
+  -c mkv \
+  -w "DisplayPort-0|DisplayPort-1;x=2560;height=1080" \
+  -k av1 \
+  -fm content \
+  -r 120 \
+  -low-power yes \
+  -a "default_output|default_input" \
+  -v no \
+  -sc "$HOME/Scripts/gpu-screen-recorder-notify.sh" \
+  -o "$HOME/Videos/Replays" &
 
 # mic loopback
 pw-loopback &
